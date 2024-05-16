7e4c84fe0830a914ba5bd2ba678ac284f5301834
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2024-05-16 08:58:34 +0530

    Tips & Tricks

---
+++ Tips-and_tricks/README.md
@@ -0,0 +1,10 @@
+WHERE vs HAVING
+- When you need to filter on individual record then use WHERE
+- When want o filter on aggregated value use HAVING with GROUP BY
+- Remember if using both. Order of exection is that WHERE filter is applied first and then HAVING filter is applied
+
+Pivot & Unpivit
+- See [pivot-unpivot.sql](pivot-unpivot.sql)
+
+
+https://www.youtube.com/playlist?list=PLBTZqjSKn0IcR6DhoLUibOG8frnWbZdSH 
---
+++ Tips-and_tricks/pivot-unpivot.sql
@@ -0,0 +1,65 @@
+-- Create Table
+create table emp_compensation (
+emp_id int,
+salary_component_type varchar(20),
+val int
+);
+insert into emp_compensation
+values (1,'salary',10000),(1,'bonus',5000),(1,'hike_percent',10)
+, (2,'salary',15000),(2,'bonus',7000),(2,'hike_percent',8)
+, (3,'salary',12000),(3,'bonus',6000),(3,'hike_percent',7);
+select * from emp_compensation;
+
+-- Pivot
+SELECT
+	emp_id,
+	SUM(CASE WHEN salary_component_type='salary' THEN val END) as salary,
+	SUM(CASE WHEN salary_component_type='bonus' THEN val END) as bonus,
+	SUM(CASE WHEN salary_component_type='hike_percent' THEN val END) as hike_percent
+INTO
+	emp_compensation_pivot
+FROM
+	emp_compensation
+GROUP BY
+	emp_id;
+	
+SELECT
+	*
+FROM
+	emp_compensation_pivot;
+
+-- Unpivot
+SELECT
+	*
+INTO
+	emp_compensation_unpivot
+FROM
+	(
+		SELECT
+			emp_id,
+			'salary' AS salary_component_type,
+			salary AS val
+		FROM
+			emp_compensation_pivot
+		UNION ALL
+		SELECT
+			emp_id,
+			'bonus' AS salary_component_type,
+			bonus AS val
+		FROM
+			emp_compensation_pivot
+		UNION ALL
+		SELECT
+			emp_id,
+			'hike_percent' AS salary_component_type,
+			hike_percent AS val
+		FROM
+			emp_compensation_pivot
+	) AS a
+ORDER BY
+	emp_id;
+	
+SELECT
+	*
+FROM
+	emp_compensation_unpivot;
