7d6791f8615f5d0b29ce05c67f9a568a52e5595a
Maaz Ahmed Ansari <70753689+maaz-ahmed-ansari@users.noreply.github.com>
2026-03-17 12:10:30 +0530

    Update 17-duplicate-job-listings.sql

--- Scripts/Easy/17-duplicate-job-listings.sql
+++ Scripts/Easy/17-duplicate-job-listings.sql
@@ -18,3 +18,13 @@ FROM
   job_count_cte
 WHERE
   job_count > 1;
+
+-- Way 2
+SELECT
+  COUNT(*)
+FROM
+  (SELECT 
+    DISTINCT company_id, title, description
+  FROM job_listings
+  GROUP BY company_id, title, description
+  HAVING COUNT(*) > 1) x;
