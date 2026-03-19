0ea8a6c9a24f43868a7da0189aaf9a03a2bc84ec
Maaz Ahmed Ansari <70753689+maaz-ahmed-ansari@users.noreply.github.com>
2026-03-19 15:09:39 +0530

    Update 26-total-drugs-sales.sql

--- Scripts/Easy/26-total-drugs-sales.sql
+++ Scripts/Easy/26-total-drugs-sales.sql
@@ -22,3 +22,12 @@ SELECT
   ) AS sale
 FROM
   total_pharmacy_sales
+
+
+-- Alternate way
+SELECT 
+  manufacturer,
+  '$' || ROUND(SUM(total_sales) / 1000000) || ' million' AS sale
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER By SUM(total_sales) DESC;
